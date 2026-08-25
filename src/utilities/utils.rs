39a1c47e142d6a3bb64f06a5ddc67eed349bb670
Iraklis Leontiadis <leontiad@gmail.com>
2026-08-25 21:33:28 +0200

    SIL-06-004 WP1: Timing side-channel on delta in yao_to_binary functionality (Info)

--- src/utilities/utils.rs
+++ src/utilities/utils.rs
@@ -1,12 +1,40 @@
 // Copyright (c) Silence Laboratories Pte. Ltd. All Rights Reserved.
 // This software is licensed under the Silence Laboratories License Agreement.
 
+use subtle::{Choice, ConstantTimeEq};
+
 use super::types::Block;
 
 /// Returns the bitwise xor, given two 128-bit blocks
 #[inline(always)]
 pub fn xor_blocks(array1: &Block, array2: &Block) -> Block {
     std::array::from_fn(|i| array1[i] ^ array2[i])
+}
+
+/// Constant-time equality of two blocks.
+#[inline(always)]
+pub fn blocks_ct_eq(a: &Block, b: &Block) -> Choice {
+    a.ct_eq(b)
+}
+
+/// Returns whether `label` equals `a` or `b`, in constant time.
+///
+/// Used when checking that a received Yao label is one of `{W₀, W₀⊕Δ}` without
+/// leaking which via early-exit `==` or short-circuit `||`.
+#[inline(always)]
+pub fn label_in_pair(label: &Block, a: &Block, b: &Block) -> bool {
+    bool::from(blocks_ct_eq(label, a) | blocks_ct_eq(label, b))
+}
+
+/// Returns whether `label` equals `f_label` or `f_label ⊕ delta`, in constant time.
+#[inline(always)]
+pub fn label_matches_wire(
+    label: &Block,
+    f_label: &Block,
+    delta: &Block,
+) -> bool {
+    let other = xor_blocks(f_label, delta);
+    label_in_pair(label, f_label, &other)
 }
 
 /// Converts a vector of boolean values to a hexadecimal string.
@@ -34,4 +62,19 @@ pub fn bool_vec_to_hex(vec: Vec<bool>) -> String {
 #[inline(always)]
 pub fn lsb(value: &Block) -> u8 {
     value[0] & 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn label_in_pair_accepts_either_endpoint() {
+        let w0 = [1u8; 16];
+        let delta = [2u8; 16];
+        let w1 = xor_blocks(&w0, &delta);
+        assert!(label_matches_wire(&w0, &w0, &delta));
+        assert!(label_matches_wire(&w1, &w0, &delta));
+        assert!(!label_matches_wire(&[0xffu8; 16], &w0, &delta));
+    }
 }
